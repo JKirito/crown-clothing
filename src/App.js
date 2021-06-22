94df74e90ca08e09ed1f90344b91e86e811c61fb
JKirito <byrxarpitsingh@gmail.com>
2021-06-22 21:59:51 +0530

    Updating App with Shop Page, COllectionPreview Component and CollectionItem Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './page/homepage/homepage.component'
+import ShopPage from './page/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path='' exact component={Homepage} />
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div >
   );
